38d63e8ed1ae8c589da9bbefdf3580016e087a10
Kei-1111 <kei_02_1111@icloud.com>
2024-08-27 01:44:47 +0900

    Trigger CI

--- composeApp/src/commonMain/kotlin/org/example/project/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/theme/Theme.kt
@@ -244,10 +244,6 @@ data class ColorFamily(
     val onColorContainer: Color
 )
 
-val unspecified_scheme = ColorFamily(
-    Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
-)
-
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
