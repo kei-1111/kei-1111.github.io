fd729e76d3c177c0b913834a6488152f1ef2809a
Kei-1111 <kei_02_1111@icloud.com>
2024-08-27 23:25:39 +0900

    update: splashscreen animation

--- composeApp/src/commonMain/kotlin/org/example/project/ui/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/splash/SplashScreen.kt
@@ -1,10 +1,14 @@
 package org.example.project.ui.splash
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -18,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -25,6 +30,7 @@ import kei_1111.composeapp.generated.resources.Res
 import kei_1111.composeapp.generated.resources.img_profile_icon
 import kotlinx.coroutines.delay
 import org.example.project.model.UiDimensions
+import org.example.project.ui.theme.NotoSansJpFamily
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
@@ -34,7 +40,23 @@ fun SplashScreen(
     val text = "Hello!!"
     var s by remember { mutableStateOf("") }
 
-    LaunchedEffect(Unit) {
+    val alphaAnim = remember { Animatable(0f) } // フェードイン用アニメーション
+    val offsetXAnim = remember { Animatable(-100f) } // スライドイン用アニメーション
+
+
+
+    LaunchedEffect(MaterialTheme.typography.headlineLarge.fontFamily == NotoSansJpFamily()) {
+
+        alphaAnim.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 1500)
+        )
+
+        offsetXAnim.animateTo(
+            targetValue = 0f,
+            animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing)
+        )
+
         text.indices.forEach { index ->
             delay(100)
             s = text.substring(0..index)
@@ -48,12 +70,16 @@ fun SplashScreen(
         contentAlignment = Alignment.Center
     ) {
         Row(
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.alpha(alphaAnim.value)
         ) {
             Image(
                 painter = painterResource(Res.drawable.img_profile_icon),
                 contentDescription = "Profile icon",
-                modifier = Modifier.size(UiDimensions.mediumIconSize).clip(CircleShape),
+                modifier = Modifier
+                    .offset(x = offsetXAnim.value.dp) // スライドインの位置指定
+                    .size(UiDimensions.mediumIconSize)
+                    .clip(CircleShape),
                 contentScale = ContentScale.Crop
             )
             if (s != "") {
