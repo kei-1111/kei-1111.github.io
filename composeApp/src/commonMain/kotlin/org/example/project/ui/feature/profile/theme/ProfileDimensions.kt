168bbd0ec7ba45cc00c47e5b135f6e5ccdba16d4
kei-1111 <kei_02_1111@icloud.com>
2025-02-19 16:59:46 +0900

    refactor: 不要になったパラメータの削除

--- composeApp/src/commonMain/kotlin/org/example/project/ui/feature/profile/theme/ProfileDimensions.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/feature/profile/theme/ProfileDimensions.kt
@@ -5,9 +5,6 @@ package org.example.project.ui.feature.profile.theme
 import androidx.compose.ui.unit.dp
 
 internal data object ProfileDimensions {
-    const val DesktopRightWeight = 1.5f
-    val CareerThickness = 5.dp
-    val CareerDividerWidth = 15.dp
     val ImageWidth = 200.dp
     const val WorkSectionDescriptionMinLines = 5
 }
