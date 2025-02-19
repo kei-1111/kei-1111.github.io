e9872cf0fcd15dafe79bceffb2a524a291c9f9d2
kei-1111 <kei_02_1111@icloud.com>
2025-02-19 16:04:46 +0900

    add: SNSSectionのUIを作成

---
+++ composeApp/src/commonMain/kotlin/org/example/project/data/SNSSet.kt
@@ -0,0 +1,27 @@
+package org.example.project.data
+
+import kei_1111.composeapp.generated.resources.Res
+import kei_1111.composeapp.generated.resources.img_github
+import kei_1111.composeapp.generated.resources.img_note
+import kei_1111.composeapp.generated.resources.img_x
+import org.example.project.model.SNS
+
+data object SNSSet {
+    val SNSs = listOf(
+        SNS(
+            image = Res.drawable.img_x,
+            name = "X",
+            url = "https://x.com/kei_1111_",
+        ),
+        SNS(
+            image = Res.drawable.img_github,
+            name = "GitHub",
+            url = "https://github.com/kei-1111",
+        ),
+        SNS(
+            image = Res.drawable.img_note,
+            name = "note",
+            url = "https://note.com/kei_1111_",
+        ),
+    )
+}
